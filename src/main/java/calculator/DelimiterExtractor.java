6b905c8d2bdab785963f1609190805d917c9f960
khs29216 <khs29216@gmail.com>
2025-10-17 05:02:00 +0900

    feat(DelimiterExtractor): add 구분자 추출 기능

---
+++ src/main/java/calculator/DelimiterExtractor.java
@@ -0,0 +1,33 @@
+package calculator;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/*
+    DelimiterExtractor 클래스
+    - 역할 : 입력한 문자열에서 구분자 추출
+    - 기능
+        1. 문자열에서 구분자 추출
+ */
+public class DelimiterExtractor {
+
+    public List<String> delimiterExtract(String input) {
+        List<String> delimiters = new ArrayList<>(Arrays.asList(",", ":"));
+        String regex = "^//([^\\d\\s])\\\\n.*";
+        if (isRegexMatch(regex, input)) {
+            Matcher matcher = Pattern.compile(regex).matcher(input);
+            if (matcher.find()) {
+                String customDelimiter = matcher.group(1);
+                delimiters = new ArrayList<>(Arrays.asList(customDelimiter));
+            }
+        }
+        return delimiters;
+    }
+
+    private boolean isRegexMatch(String regex, String input) {
+        return Pattern.matches(regex, input);
+    }
+}
