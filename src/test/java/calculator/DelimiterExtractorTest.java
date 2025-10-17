1866fe7443d7bdd58c5a82f702d016443d55c8f7
khs29216 <khs29216@gmail.com>
2025-10-17 21:40:50 +0900

    test(DelimiterExtractor): 구분자 추출 테스트 추가 -기본 구분자를 사용하는 경우 테스트 진행 -커스텀 구분자를 지정한 경우 테스트 진행

---
+++ src/test/java/calculator/DelimiterExtractorTest.java
@@ -0,0 +1,42 @@
+package calculator;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DelimiterExtractorTest {
+
+    @Test
+    void 기본_구분자_추출() {
+        // given
+        List<String> testDelimiter = new ArrayList<>(Arrays.asList(",", ":"));
+        String testInput = "1,2:3";
+
+        DelimiterExtractor delimiterExtractor = new DelimiterExtractor();
+
+        // when
+        List<String> result = delimiterExtractor.delimiterExtract(testInput);
+
+        // then
+        assertEquals(testDelimiter, result);
+    }
+
+    @Test
+    void 커스텀_구분자_추출() {
+        // given
+        List<String> testDelimiter = new ArrayList<>(Arrays.asList("d"));
+        String testInput = "//d\\n1d2d3";
+
+        DelimiterExtractor delimiterExtractor = new DelimiterExtractor();
+
+        // when
+        List<String> result = delimiterExtractor.delimiterExtract(testInput);
+
+        // then
+        assertEquals(testDelimiter, result);
+    }
+}
