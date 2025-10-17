ff2082569800f54bcd7fb3196dd823db829e5e72
khs29216 <khs29216@gmail.com>
2025-10-17 21:09:23 +0900

    refactor(IOManager): 입력 메서드를 Scanner.nextLine()에서 Console.readLine()로 변경

--- src/main/java/calculator/IOManager.java
+++ src/main/java/calculator/IOManager.java
@@ -1,6 +1,6 @@
 package calculator;
 
-import java.util.Scanner;
+import camp.nextstep.edu.missionutils.Console;
 
 /*
     IOManager 클래스
@@ -12,9 +12,8 @@ import java.util.Scanner;
 public class IOManager {
     // 사용자의 입력을 받는 매서드
     public String input() {
-        Scanner scanner = new Scanner(System.in);
         System.out.println("덧셈할 문자열을 입력해 주세요.");
-        return scanner.nextLine();
+        return Console.readLine();
     }
 
     // 결과를 출력하는 메서드
