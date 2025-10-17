c89da6ac9ba9bb5f3920823ce36d91b1db28385a
khs29216 <khs29216@gmail.com>
2025-10-17 23:09:32 +0900

    feat(NumberExtractor): 숫자 추출 기능 추가

---
+++ src/main/java/calculator/NumberExtractor.java
@@ -0,0 +1,54 @@
+package calculator;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+
+
+/*
+    NumberExtractor 클래스
+    - 역할 : 입력한 문자열에서 숫자들 추출
+    - 기능
+        1. 문자열에서 숫자 추출
+ */
+public class NumberExtractor {
+
+    public List<Integer> extractNumber(String input, List<String> delimiter) {
+        // 커스텀 구분자를 지정한 경우, 숫자를 입력한 문자열 부분만 추출
+        input = extractNumberPart(input);
+
+        // 문자열에서 숫자들 각각 분리
+        String[] numbers = splitNumber(input, delimiter);
+
+        List<Integer> extractNumbers = new ArrayList<>();
+        for (String num : numbers) {
+            if (!num.matches("\\d+")) {
+                throw new IllegalArgumentException("잘못된 입력입니다. " + num);
+            }
+            extractNumbers.add(Integer.parseInt(num));
+        }
+
+        return extractNumbers;
+    }
+
+    private static String extractNumberPart(String input) {
+        String regex = "^//([^\\d\\s])\\\\n(.*)";
+        Matcher matcher = Pattern.compile(regex).matcher(input);
+
+        // 커스텀 구분자를 지정한 경우
+        if (matcher.find()) {
+            input = matcher.group(2);
+        }
+
+        return input;
+    }
+
+    private static String[] splitNumber(String input, List<String> delimiter) {
+        String delimiterRegex = delimiter.stream()
+                .map(Pattern::quote)
+                .collect(Collectors.joining("|"));
+        return input.split(delimiterRegex);
+    }
+}
