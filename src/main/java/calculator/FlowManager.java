a373b906870a0590466bb895282a5700cf3ca1e8
khs29216 <khs29216@gmail.com>
2025-10-19 00:04:19 +0900

    fix(FlowManager): 사용자 잘못된 입력 시 IllegalArgumentException 발생 -문제의 요구사항에 맞게 수정 -사용자 잘못된 입력 시 IllegalArgumentException 발생 후 강제 종료

--- src/main/java/calculator/FlowManager.java
+++ src/main/java/calculator/FlowManager.java
@@ -8,33 +8,27 @@ import java.util.List;
     - 기능
         1. 프로그램 전체 흐름 제어
             - 입력 -> 구분자 추출 -> 숫자 추출 -> 계산 -> 출력
-        2. 예외 발생 시 사용자에게 오류 메시지를 출력
  */
 public class FlowManager {
 
     void execute() {
-        try {
-            // 1. 사용자 입력
-            IOManager ioManager = new IOManager();
-            String input = ioManager.input();
+        // 1. 사용자 입력
+        IOManager ioManager = new IOManager();
+        String input = ioManager.input();
 
-            // 2. 입력된 문자열에서 구분자 추출
-            DelimiterExtractor delimiterExtractor = new DelimiterExtractor();
-            List<String> strings = delimiterExtractor.delimiterExtract(input);
+        // 2. 입력된 문자열에서 구분자 추출
+        DelimiterExtractor delimiterExtractor = new DelimiterExtractor();
+        List<String> strings = delimiterExtractor.delimiterExtract(input);
 
-            // 3. 입력된 문자열에서 숫자 추출
-            NumberExtractor extractor = new NumberExtractor();
-            List<Double> list = extractor.extractNumber(input, strings);
+        // 3. 입력된 문자열에서 숫자 추출
+        NumberExtractor extractor = new NumberExtractor();
+        List<Double> list = extractor.extractNumber(input, strings);
 
-            // 4. 추출된 숫자들 계산
-            Calculator calculator = new Calculator();
-            Double result = calculator.calculate(list);
+        // 4. 추출된 숫자들 계산
+        Calculator calculator = new Calculator();
+        Double result = calculator.calculate(list);
 
-            // 5. 결과 출력
-            ioManager.printResult(result);
-        } catch (IllegalArgumentException e) {
-            System.out.println(e.getMessage());
-        }
-
+        // 5. 결과 출력
+        ioManager.printResult(result);
     }
 }
